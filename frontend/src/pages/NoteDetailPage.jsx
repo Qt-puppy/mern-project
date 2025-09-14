9f893d4d485bc06e8816fe16aa594d5057889338
Qt-puppy <kavomoudgil22@gmail.com>
2025-09-14 12:02:06 +0530

    Fixing logs

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -13,7 +13,6 @@ const NoteDetailPage = () => {
   const navigate = useNavigate();
 
   const { id } = useParams();
-  console.log({ id });
 
   useEffect(() => {
     const fetchNote = async () => {
